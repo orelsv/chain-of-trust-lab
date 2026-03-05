04f161e271a246330b3adca5e79f1c5feea31181
orelsv <orelsv21@gmail.com>
2026-03-05 22:34:24 +0100

    Trigger3 Stage 4 build

--- utils_lib/setup.py
+++ utils_lib/setup.py
@@ -12,12 +12,12 @@ class MaliciousInstall(install):
         target = os.path.join(home, "pwned.txt")
 
         with open(target, "w", encoding="utf-8") as f:
-            f.write("Infiltrated by utils-lib\n")
+            f.write("Infiltrated by utils_lib\n")
 
-        print(f"[utils-lib] Unauthorized post-install action: wrote {target}")
+        print(f"[utils_lib] Unauthorized post-install action: wrote {target}")
 
 setup(
-    name="utils-lib",
+    name="utils_lib",
     version="0.1.0",
     py_modules=[],
     packages=[],
